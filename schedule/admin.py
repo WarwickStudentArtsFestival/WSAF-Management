4841554f3523868465ce8aa5531966d811fcc033
Adam Skrzymowski <35405850+adamskrz@users.noreply.github.com>
2024-05-17 00:16:13 +0100

    Load events in and improve admin

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Event, EventInstance, Venue, Organisation, Category
 
 class EventInstanceInline(admin.TabularInline):
@@ -8,17 +7,22 @@ class EventInstanceInline(admin.TabularInline):
     extra = 1
 
 
-# in table of events, show str, prefered_occurrences, and a count of event instances
-
 class EventAdmin(admin.ModelAdmin):
     inlines = [EventInstanceInline]
 
-    list_display = ('__str__', 'preferred_occurrences')
+    list_display = ('__str__', 'preferred_occurrences', 'assigned_instances')
 
+    def assigned_instances(self, obj):
+        return obj.eventinstance_set.count()
+
+class EventInstanceAdmin(admin.ModelAdmin):
+    list_display = ('event', 'venue', 'start', 'end', 'venue', 'parent')
+
+    ordering = ('start',)
 
 
 admin.site.register(Organisation)
 admin.site.register(Venue)
 admin.site.register(Event, EventAdmin)
-admin.site.register(EventInstance)
+admin.site.register(EventInstance, EventInstanceAdmin)
 admin.site.register(Category)
--- schedule/models.py
+++ schedule/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Organisation(models.Model):
     name = models.CharField(max_length=200)
@@ -9,7 +10,7 @@ class Organisation(models.Model):
 
 class Event(models.Model):
     organisation = models.ForeignKey('Organisation', on_delete=models.CASCADE, null=True, blank=True)
-    submission_id = models.IntegerField(null=True)
+    submission_id = models.IntegerField(null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     estimated_duration = models.DurationField(null=True, blank=True)
@@ -19,10 +20,14 @@ class Event(models.Model):
     categories = models.ManyToManyField('Category', blank=True)
 
     def __str__(self):
-        if self.organisation is None:
-            return f'{self.submission_id} - {self.title}'
+        name_string = ""
+        if self.submission_id is not None:
+            name_string += f'{self.submission_id} - '
+        name_string += self.title
+        if self.organisation is not None:
+            name_string += f' ({self.organisation})'
 
-        return f'{self.submission_id} - {self.title} ({self.organisation})'
+        return name_string
 
 class EventInstance(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
@@ -32,7 +37,10 @@ class EventInstance(models.Model):
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.event} at {self.venue} from {self.start} to {self.end}'
+        start_time = timezone.localtime(self.start).strftime('%A %H:%M')
+        end_time = timezone.localtime(self.end).strftime('%H:%M')
+        return f'{self.event} at {self.venue} from {start_time} to {end_time}'
+
 
 class Venue(models.Model):
     name = models.CharField(max_length=200)
